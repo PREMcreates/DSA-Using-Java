3f099eefd01f221eb0f8c6d1e9a7af341b307b08
fullstackNINJA05 <prempanchal0501@gmail.com>
2026-07-18 00:52:54 +0530

    added first code

---
+++ src/maxofthreenum.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class maxofthreenum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        int ans = Math.max(c, Math.max(a,b));
+        System.out.println(ans);
+    }
+}
